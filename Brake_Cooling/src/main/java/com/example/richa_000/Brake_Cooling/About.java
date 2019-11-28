9b874b867c6327f3d8720e2793ddbf412a63b08a
tubbmeister <richard.hoskison@btinternet.com>
2019-11-28 21:29:16 +0000

    second

---
+++ Brake_Cooling/src/main/java/com/example/richa_000/Brake_Cooling/About.java
@@ -0,0 +1,23 @@
+package com.example.richa_000.Brake_Cooling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.widget.TextView;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView aboutView = (TextView) findViewById(R.id.textViewVersion);
+        String numberTemp = BuildConfig.VERSION_NAME;
+        aboutView.setText("Version number " + numberTemp);
+    }
+
+
+
+}
